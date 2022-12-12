d0037a3569b8a7d6942d0005937f097414d9a595
Svetlana <isaeva322@mail.ru>
2022-12-12 21:12:06 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    //Метод 1 Задание 1
+    //Расчет високосного года
+    public static boolean isLeapYear (int year) {
+       return year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ;
+    }
+
+    //Метод 2 Задание 1
+    //Печать високосного/невисокосного года
+    public static void printLeapOrNotLeapYear (int year){
+        if (isLeapYear(year)){
+            System.out.println( year + " Год является всокосным ");
+            return;
+        }
+        System.out.println(  year + " Год не является всокосным ");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Задание 1
+        System.out.println( " Задание 1");
+        printLeapOrNotLeapYear(2014);
+
+
     }
 }
