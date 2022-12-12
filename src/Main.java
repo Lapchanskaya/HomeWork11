3f7fc3d460f7e1f5ba4707ba2d8197447d5253d4
Svetlana <isaeva322@mail.ru>
2022-12-12 21:46:17 +0300

    Выполнено задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+
+import java.time.LocalDate;
+
 public class Main {
-    //Метод 1 Задание 1
+    private final static int currentYear = LocalDate.now().getYear();
+    // Задание 1 Метод 1
     //Расчет високосного года
     public static boolean isLeapYear (int year) {
        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ;
     }
 
-    //Метод 2 Задание 1
+    // Задание 1 Метод 1
     //Печать високосного/невисокосного года
     public static void printLeapOrNotLeapYear (int year){
         if (isLeapYear(year)){
@@ -14,10 +18,46 @@ public class Main {
         }
         System.out.println(  year + " Год не является всокосным ");
     }
+
+
+
+
+    //Задание 2 Метод 1
+    // определение андройда или айфона
+    public static String identificationOs ( int versionOS) {
+        if (versionOS == 0) {
+            return "IOS";
+        } else if (versionOS == 1) {
+            return "Android";
+        } else {
+          throw new RuntimeException(" Такой операционной системы не существует");
+        }
+    }
+
+    //Задание 2 Метод 2
+    // Определение текущего года
+    public static boolean isNowYear(int year){
+        return year == currentYear;
+    }
+    // Задание 2 Метод 3
+    // Определение версии ос
+    public static void printVersionOS( int year, int versionOS){
+        if (isNowYear(year)){
+            System.out.println( " Установите полную версию для Вашего " + identificationOs(versionOS));
+        }else{
+            System.out.println(" Установите lite версию для Вашего " + identificationOs(versionOS)) ;
+        }
+    }
+
+
     public static void main(String[] args) {
     // Задание 1
         System.out.println( " Задание 1");
         printLeapOrNotLeapYear(2014);
+
+    // Задание 2
+        System.out.println( " Задание 2");
+        printVersionOS( 2022, 0);
 
 
     }
